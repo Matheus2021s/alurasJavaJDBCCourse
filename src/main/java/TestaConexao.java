862414b906e4e4205444706c6336f8797873e5a9
matheus <matheusmartinsdacosta21@gmail.com>
2021-07-24 23:39:48 -0300

    Creating an connection factory to handle de requests of a new instance of connection to database

--- src/main/java/TestaConexao.java
+++ src/main/java/TestaConexao.java
@@ -1,14 +1,12 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TestaConexao {
 
 	public static void main(String[] args) throws SQLException {
-		Connection connection = 
-				DriverManager
-				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimeZone=true&serverTimeZone=UTC", "root", "SenhaRoot@!123");
-
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperarConexao();
+		
 		connection.close();
 	}
 }
